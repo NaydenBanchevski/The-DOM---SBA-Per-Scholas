14a98a29822ee8ea26f3f1bc18e4f5d254ea323b
Nayden Banchevski <163778420+NaydenBanchevski@users.noreply.github.com>
2024-08-16 21:03:51 -0500

    Add Custom Cursor function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const navUl = document.getElementById("nav-ul");
+let navUl = document.getElementById("nav-ul");
 
 const navLinks = [
   { label: "Home", href: "/" },
@@ -16,3 +16,51 @@ navLinks.forEach((link) => {
 });
 
 navUl.classList.add("menu");
+
+function createCircles() {
+  const container = document.createElement("div");
+  for (let i = 0; i < 50; i++) {
+    const circle = document.createElement("div");
+    circle.classList.add("circle");
+    container.appendChild(circle);
+  }
+  document.body.appendChild(container);
+}
+
+createCircles();
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+circles.forEach(function (circle) {
+  circle.x = 0;
+  circle.y = 0;
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+    circle.style.opacity = (circles.length - index) / circles.length;
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.2;
+    y += (nextCircle.y - y) * 0.2;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
