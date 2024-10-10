1f81d39caac7638ce7bd02156f76435e4d35312f
Nayden Banchevski <163778420+NaydenBanchevski@users.noreply.github.com>
2024-10-10 15:36:08 -0500

    Fixed responsiveness

--- app.js
+++ app.js
@@ -76,54 +76,54 @@ document.querySelectorAll(".nav-links").forEach((link) => {
   });
 });
 
-// custom cursor
-function createCircles() {
-  const container = document.createElement("div");
-  for (let i = 0; i < 50; i++) {
-    const circle = document.createElement("div");
-    circle.classList.add("circle");
-    container.appendChild(circle);
-  }
-  document.body.appendChild(container);
-}
+// // custom cursor
+// function createCircles() {
+//   const container = document.createElement("div");
+//   for (let i = 0; i < 50; i++) {
+//     const circle = document.createElement("div");
+//     circle.classList.add("circle");
+//     container.appendChild(circle);
+//   }
+//   document.body.appendChild(container);
+// }
 
-createCircles();
+// createCircles();
 
-const coords = { x: 0, y: 0 };
-const circles = document.querySelectorAll(".circle");
+// const coords = { x: 0, y: 0 };
+// const circles = document.querySelectorAll(".circle");
 
-circles.forEach(function (circle) {
-  circle.x = 0;
-  circle.y = 0;
-});
+// circles.forEach(function (circle) {
+//   circle.x = 0;
+//   circle.y = 0;
+// });
 
-window.addEventListener("mousemove", function (e) {
-  coords.x = e.clientX;
-  coords.y = e.clientY;
-});
+// window.addEventListener("mousemove", function (e) {
+//   coords.x = e.clientX;
+//   coords.y = e.clientY;
+// });
 
-function animateCircles() {
-  let x = coords.x;
-  let y = coords.y;
+// function animateCircles() {
+//   let x = coords.x;
+//   let y = coords.y;
 
-  circles.forEach(function (circle, index) {
-    circle.style.left = x - 12 + "px";
-    circle.style.top = y - 12 + "px";
+//   circles.forEach(function (circle, index) {
+//     circle.style.left = x - 12 + "px";
+//     circle.style.top = y - 12 + "px";
 
-    circle.style.scale = (circles.length - index) / circles.length;
-    circle.style.opacity = (circles.length - index) / circles.length;
-    circle.x = x;
-    circle.y = y;
+//     circle.style.scale = (circles.length - index) / circles.length;
+//     circle.style.opacity = (circles.length - index) / circles.length;
+//     circle.x = x;
+//     circle.y = y;
 
-    const nextCircle = circles[index + 1] || circles[0];
-    x += (nextCircle.x - x) * 0.2;
-    y += (nextCircle.y - y) * 0.2;
-  });
+//     const nextCircle = circles[index + 1] || circles[0];
+//     x += (nextCircle.x - x) * 0.2;
+//     y += (nextCircle.y - y) * 0.2;
+//   });
 
-  requestAnimationFrame(animateCircles);
-}
+//   requestAnimationFrame(animateCircles);
+// }
 
-animateCircles();
+// animateCircles();
 // Register and Login  functionality
 // username validation
 function userNameValidation() {
